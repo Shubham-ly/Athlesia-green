cf76e0061dff68a0258495ec717884df7dd0d273
ANAGHA-LY <anagha@ly.design>
2022-11-26 11:09:36 +0530

    added sound

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,9 @@ module.exports = {
         base: ['16px', '24px'],
         lg: ['24px', '21px'],
         xl: ['24px', '32px'],
+      },
+      animation: {
+        'animation': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',  
       }
     },
   },
