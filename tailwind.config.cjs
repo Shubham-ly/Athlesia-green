a1de648de48537e5f34c816f78f49d56fce168e1
Shubham-ly <shubham.m@ly.design>
2022-11-26 13:34:34 +0530

    Merged Anagha's audio file and fixed footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,15 @@ module.exports = {
     extend: {
       colors: {
         primary: "#010605",
+        green: {
+          400: "#08E163",
+        },
+        blue: {
+          400: "#00A3FF",
+        },
+        red: {
+          400: "#B32D2A",
+        },
       },
       fontFamily: {
         japanese: "Japanese",
@@ -15,6 +24,9 @@ module.exports = {
         base: ["16px", "24px"],
         lg: ["24px", "21px"],
         xl: ["24px", "32px"],
+      },
+      animation: {
+        animation: "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
     },
   },
